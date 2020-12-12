13c356b851661b9eb2d87ae77af342a1db0aedea
Woo <leo9634@naver.com>
2020-12-12 09:45:30 -0500

    google oauth configured

---
+++ src/main/java/com/awswebservice/config/auth/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.awswebservice.config.auth;
+
+
+//import com.wavestoked.domain.user.Role;
+import com.awswebservice.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomUserOAuth2UserService customUserOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/","/**", "/css/**", "/images/**",
+                        "/js/**", "/h2-console/**").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customUserOAuth2UserService);
+        super.configure(http);
+    }
+}
---
+++ src/main/java/com/awswebservice/domain/user/Role.java
@@ -0,0 +1,24 @@
+package com.awswebservice.domain.user;
+
+// inside the domain repo,
+
+// user -> package/dir
+    // User -> Entity class
+    // Role -> enum class
+    // UserRepository -> extended from jpa repository
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+
+    GUEST("ROLE_GUEST", "손님"),
+    USER("ROLE_USER", "일반 사용자");
+
+    private final String key;
+    private final String title;
+
+}
