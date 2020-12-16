51acb03a69387f2863f62ef070e987abe0b0a94c
Woo-Park <wp503@nyu.edu>
2020-12-16 16:50:32 +0900

    jwt 연습

---
+++ src/main/java/com/awswebservice/config/jwt/JwtFactory.java
@@ -0,0 +1,47 @@
+package com.awswebservice.config.jwt;
+
+import com.auth0.jwt.algorithms.Algorithm;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.auth0.jwt.JWT;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+
+// import 생략
+
+@RequiredArgsConstructor
+@Component
+public class JwtFactory { // aka JwtTokenProvider JWT 토큰을 생성 및 검증 모듈
+    private static final Logger log = LoggerFactory.getLogger(JwtFactory.class);
+    // 2.
+    private static String signingKey = "jwttest";
+
+    // 1.
+    public String generateToken(AccountContext account) {
+        String token = null;
+        try {
+            token = JWT.create()
+                    .withIssuer("wooinabillion")
+                    .withClaim("USERNAME", account.getAccount().getUserId())
+                    .withClaim("USER_ROLE", account.getAccount().getUserRole().getKey())
+                    .withClaim("EXP", new Date(System.currentTimeMillis() + 864000000))
+                    .sign(generateAlgorithm());
+        } catch(Exception e) {
+            e.printStackTrace();
+            log.error(e.getMessage());
+        }
+
+        return token;
+    }
+
+    // 2.
+    private Algorithm generateAlgorithm() throws UnsupportedEncodingException {
+        return Algorithm.HMAC256(signingKey);
+    }
+
+
+}
